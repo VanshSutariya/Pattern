24c0fb6218f7375af609c2b995a5bb2d4beb74a1
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-02 11:33:56 +0530

    this is my first commit in pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,18 @@
+class Pattern1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j<=i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
