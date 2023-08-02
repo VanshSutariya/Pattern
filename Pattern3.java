f0cd6890b8abe145d7b0f3f9289064e581cfaa2b
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-02 11:43:04 +0530

    this is my first commit in Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,21 @@
+public class Pattern3 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            int k = 1;
+            for (int j = 1; j <= 9; j++) {
+                if (j >= 6 - i && j <= 4 + i && k == 1) {
+                    System.out.print("*");
+                    k = 0;
+                } else {
+                    System.out.print(" ");
+                    k = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
