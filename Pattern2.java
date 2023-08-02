eb8c412cbd261ac5f25ccdfd798bb2a5cec15be5
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-02 11:40:19 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,3 +1,14 @@
+/*
+Create  this  Pattern:
+    *    
+   ***
+  *****
+ *******
+*********
+
+*/
+
+
 public class Pattern2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
