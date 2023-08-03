c3e9b78d6cd34308b41fda9e91eda4769f1c8a29
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-03 16:26:20 +0530

    this is my first commit in pattern6.java

---
+++ Pattern6.java
@@ -0,0 +1,24 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        char k;
+        for (int i = 1; i <= 4; i++) {
+            k = 'A';
+
+            for (int j = 1; j <= 7; j++) {
+                if (j <= 5 - i || j >= 3 + i) {
+                    if (j >= 4) {
+                        System.out.print(k);
+                        k--;
+                    } else {
+                        System.out.print(k);
+                        k++;
+                    }
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
