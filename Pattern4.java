258cf676e9ae139e9245c0fedb57651ce8b58998
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-03 16:21:39 +0530

    this is my first commit in Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,14 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (j <= 6 - i || j >= 4 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
