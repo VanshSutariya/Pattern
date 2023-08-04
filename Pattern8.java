004a48a335924dab6da8662fee80413b1711f755
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-04 18:45:19 +0530

    it is my firt commit in Pattern8,java

---
+++ Pattern8.java
@@ -0,0 +1,37 @@
+// public class Pattern8 {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 7; i++) {
+//             if (i < 5) {
+//                 for (int j = 1; j <= i; j++) {
+//                     System.out.print("*");
+//                 }
+//             } else {
+//                 for (int j = 1; j <= 8 - i; j++)
+//                     System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        int k = 0;
+        for (int i = 1; i <= 7; i++) {
+            if (i <= 4) {
+                k++;
+            } else {
+                k--;
+            }
+
+            for (int j = 1; j <= 4; j++) {
+                if (j <= k) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
