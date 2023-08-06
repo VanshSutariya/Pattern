8b4ffbc16aa5dbbefbeba8d1f62847434cc22e5c
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-06 09:17:15 +0530

    it is my first commit in this file

---
+++ Pattern12.java
@@ -0,0 +1,52 @@
+// public class Pattern12 {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 9; i++) {
+//             int k = 1;
+//             if (i <= 5) {
+//                 for (int j = 1; j <= 5; j++) {
+//                     if (j > 5 - i) {
+//                         System.out.print(k);
+//                         k++;
+//                     } else
+//                         System.out.print(" ");
+//                 }
+//             } else {
+//                 for (int j = 1; j <= 5; j++) {
+//                     if (j >= i - 4) {
+//                         System.out.print(k);
+//                         k++;
+//                     } else
+//                         System.out.print(" ");
+//                 }
+//             }
+
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Another Method 
+
+public class Pattern12 {
+    public static void main(String args[]) {
+        int k = 0;
+        for (int i = 1; i <= 9; i++) // i is denoted by row.
+        {
+            if (i < 6) {
+                k++;
+            } else {
+                k--;
+            }
+            int x = 1;
+            for (int j = 1; j <= 5; j++) // j is denoted by column.
+            {
+                if (j >= 6 - k) {
+                    System.out.print(x);
+                    x++;
+                } else
+                    System.out.print(" ");
+            }
+            System.out.println(); // that is the logic of only 5 lines print.
+        }
+    }
+}
