1568912c4b5d4d4397b434e45b9b06e6685e2e9d
Vansh Sutariya <119673252+VanshSutariya@users.noreply.github.com>
2023-08-07 18:26:26 +0530

    this is my first commit in this file

---
+++ Pattern13.java
@@ -0,0 +1,18 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if (j == i || j == 8 - i) {
+                    if (j == 8 - i) {
+                        System.out.print("/");
+                    } else {
+                        System.out.print("\\");
+                    }
+
+                } else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
